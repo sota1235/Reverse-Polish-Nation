4bab732a9b2bf41933271efb36d46c555a0c5508
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-12-16 17:49:03 +0900

    fix rpn.js

--- rpn.js
+++ rpn.js
@@ -6,22 +6,51 @@
 
 var stac[] = document.getElementById("num").split(" ");
 var i = 0;
+var sum = 0; // Answer
 
 // エラー処理
-if(stac[0] = /* 数字か否か */) {
+if(stac[0].match(/[^0-9]/g)) {
   alert("最初に２つの数字を入力してください");
-} else if(stac[1] = /* 数字か否か */){
+  return;
+} else if(stac[1].match(/[^0-9]/g)) {
   alert("少なくとも２つの数字を入力してください");
+  return;
 }
 
-while(/* 文字列を全て洗い出し */){
-  if(stac[i]==/* 数字、記号以外に何か混ざってるか否か */){
+while(stac.length => 0){
+  if(stac[i].match(/[^0-9+-*\/]/g)){
     alert("無効な文字列が含まれています");
+    return;
   }
-  if(stac[i]==/* 数字か否か */){
+
+  if(stac[i].match(/[0-9]/g)){
     i++;
+    // 最後の文字が数字ならエラー
     if(i==stac.length){
       alert("記号を入れてください ('+','-','*','/')");
+      return;
+    }
+  } else {
+    // 記号の時の処理
+    swich(stac[i]){
+      case "+" :
+        sum = stac[i-1] + stac[i-2];
+        break;
+      case "-" :
+        sum = stac[i-1] - stac[i-2];
+        break;
+      case "*" :
+        sum = stac[i-1] * stac[i-2];
+        break;
+      case "/" :
+        sum = stac[i-1] / stac[i-2];
+        break;
+      default:
+        break;
+    }
+    for(var j=0;j<3;j++){
+      stac.pop();
     }
   }
+  document.getElementById('answer').innerHTML = "<p>Answer:"+sum+"</p>";
 }
