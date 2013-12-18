50f40226eebb345de201b6dd549517f8756fced1
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-12-18 17:32:20 +0900

    fix index.html / submitを押した時にページ遷移しないよう編集

--- rpn.js
+++ rpn.js
@@ -5,10 +5,10 @@
  */
 
 function calc(form){
-    var stac = new Array();
-    stac = form.num.value.toString().split(' ');
-    var i = 0;
-    var sum = 0; // Answer  
+  var stac = new Array();
+  stac = form.num.value.toString().split(' ');
+  var i = 0;
+  var sum = 0; // Answer
 
   // エラー処理
   if(stac[0].match(/[^0-9]/g)) {
@@ -17,13 +17,13 @@ function calc(form){
   } else if(stac[1].match(/[^0-9]/g)) {
     alert("少なくとも２つの数字を入力してください");
     return;
-  } 
+  }
 
   while(stac.length > 0){
     if(stac[i].match(/[^0-9\+\-\*\/]/g)){
       alert("無効な文字列が含まれています");
       return;
-    } 
+    }
 
     if(stac[i].match(/[0-9]/g)){
       i++;
